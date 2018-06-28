d9fb5ca985cac1f4e0af45fbb97091e3a97e026b
Mia Ćaćić <cacic.miaa@gmail.com>
2018-06-28 13:12:52 +0200

    Make a player & 3 enemies appear on screen

--- js/app.js
+++ js/app.js
@@ -1,35 +1,47 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
+class Enemy { 
+	constructor(x,y){
+		this.sprite = 'images/enemy-bug.png';
+	    this.x = x;
+		this.y = y;
+		// Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
+	}
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+	update(dt) { 
+// You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
+	}
+	// Draw the enemy on the screen, required method for game
+	render() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
+class Player {
+	constructor(x,y) {
+		this.sprite = 'images/char-boy.png';
+		this.x = x;
+		this.y = y;
+	}
+	update(){}
+	render(){
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+	handleInput(){}
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+}
 
+
+// Place all enemy objects in an array called allEnemies
+allEnemies= [new Enemy(-100,63), new Enemy(0, 146), new Enemy(0,229)];
+// Place the player object in a variable called player
+const player = new Player(200,320);
 
 
 // This listens for key presses and sends the keys to your
